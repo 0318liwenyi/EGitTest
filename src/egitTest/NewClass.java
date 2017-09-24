49d68a571f6c9febb90e3895d20f1214ebea6fbc
liwenyi0318 <liwenyi0318@163.com>
2017-09-24 19:51:24 +0800

    EGit Test
    Signed-off-by: liwenyi0318 <liwenyi0318@163.com>

---
+++ src/egitTest/NewClass.java
@@ -0,0 +1,25 @@
+package egitTest;
+
+public class NewClass {
+	private String EGit;
+	private String name;
+	public String getEGit() {
+		return EGit;
+	}
+	public void setEGit(String eGit) {
+		EGit = eGit;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "NewClass [EGit=" + EGit + ", name=" + name + "]";
+	}
+	
+	
+
+}
